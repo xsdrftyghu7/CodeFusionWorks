502752b1b0266ba3cb96b1b1e2df6d2304abde4d
xsdrftyghu7 <gs1ssiniboi@hotmail.com>
2024-05-05 09:35:01 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const pullAtValue = (arr, pullArr) => {
-  let removed = [],
-    pushToRemove = arr.forEach((v, i) =>
-      pullArr.includes(v) ? removed.push(v) : v,
-    ),
-    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
-  arr.length = 0;
-  mutateTo.forEach((v) => arr.push(v));
-  return removed;
-};
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
