ea297e65add09791a04ab84537a8b82b81a71723
xsdrftyghu7 <gs1ssiniboi@hotmail.com>
2024-05-06 20:43:47 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function sumArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0);
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
 }
