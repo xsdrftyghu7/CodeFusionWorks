6d03ffae1bd18c39cebaa6090765c4502b0d974d
xsdrftyghu7 <gs1ssiniboi@hotmail.com>
2024-05-10 14:45:14 +0700

    hello

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-function longestPalindromeSubseq(s) {
-  const n = s.length;
-  const dp = Array.from(Array(n), () => Array(n).fill(0));
-  for (let i = 0; i < n; i++) {
-    dp[i][i] = 1;
-  }
-  for (let len = 2; len <= n; len++) {
-    for (let i = 0; i < n - len + 1; i++) {
-      const j = i + len - 1;
-      if (s[i] === s[j]) {
-        dp[i][j] = 2 + dp[i + 1][j - 1];
-      } else {
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-  return dp[0][n - 1];
-}
+// Alice should have the initial balance minus tranfer amount
+if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
+  throw new Error("Alice's balance after transfer is incorrect");
+
+// Sponsor should have the initial balance minus gas
+if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
+  throw new Error("Sponsor's balance after transfer is incorrect");
